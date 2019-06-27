48b47751d85c67f247b2b9bcc6ba1f75f422954f
Ashar Mubasir <asharmubasir@Ashars-MacBook-Pro.local>
2019-06-27 13:50:53 +0700

    Fix unicorn setup

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,5 +1,7 @@
 set :application, "your_app_name"
 
+set :unicorn_service_name, 'your_app_name'
+
 append :linked_files, "config/credentials/staging.key", "config/credentials/staging.yml.enc", "config/unicorn/staging.rb"
 
 set :rvm_type, :auto
